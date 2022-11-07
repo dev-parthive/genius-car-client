8be9cb94bfff3e7b0e6cc6c3f0846b8369a11451
dev-parthive <dev-parthive>
2022-11-07 18:16:23 +0600

    carousel added

---
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import BannerItem from './BannerItem';
+// import img1 from 'https://ibb.co/D4gLn65';
+// import img2 from 'https://ibb.co/2M414d6'
+// import img3 from 'https://ibb.co/xSQRKjK'
+// import img4 from 'https://ibb.co/090tmyk'
+// import img5 from 'https://ibb.co/KmZDq2C'
+// import img6 from 'https://ibb.co/XDJFb03';
+import './Banner.css'
+
+
+
+const bannerData = [
+    {
+        image: 'https://i.ibb.co/d2PgZgL/1.jpg',
+        prev: 6,
+        id: 1,
+        next: 2
+    },
+    {
+        image: 'https://i.ibb.co/mcmtCF5/2.jpg',
+        prev: 1,
+        id: 2,
+        next: 3
+    },
+    {
+        image: 'https://i.ibb.co/wBxrfmS/3.jpg' , 
+        prev: 2,
+        id: 3,
+        next: 4
+    },
+    {
+        image: 'https://i.ibb.co/pWnDt50/4.jpg',
+        prev: 3,
+        id: 4,
+        next: 5
+    },
+    {
+        image: 'https://i.ibb.co/PMpG5w2/5.jpg' , 
+        prev: 4,
+        id: 5,
+        next: 6
+    },
+    {
+        image: 'https://i.ibb.co/QpwDf3T/6.jpg',
+        prev: 5,
+        id: 6,
+        next: 1
+    }
+]
+const Banner = () => {
+    return (
+        <div className="carousel w-full">            
+
+            {
+                bannerData.map( slide => <BannerItem  slide={slide}></BannerItem>)
+            }
+        {/* <div id="slide2" className="carousel-item relative w-full">
+          <img alt='img' src={img2} className="w-full" />
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5  bottom-0">
+            <a href="#slide1" className="btn btn-circle m-3">❮</a> 
+            <a href="#slide3" className="btn btn-circle m-3">❯</a>
+          </div>
+        </div>  */}
+
+      </div>
+    );
+};
+
+export default Banner;
